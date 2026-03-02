906bbba78bdbb978cfd8cb12fa7ffa6ccbf489d9
Perum Alam Madani <ictkece@gmail.com>
2026-03-02 14:21:25 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,12 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        proxy: {
+          '/api': {
+            target: `http://localhost:${env.TIDB_API_PORT || 5174}`,
+            changeOrigin: true,
+          },
+        },
       },
       plugins: [react()],
       define: {
